35658833f0c6d7e679812807a951698f8b3b0137
Brucechen13 <chenchi_94@163.com>
2015-04-01 21:55:11 +0800

    first commit
    nodejs to commit

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/users', function(err){
+	if(!err){
+		console.log('connect to mongodb');
+	}else{
+		throw err;
+	}
+});
+var Schema = mongoose.Schema, 
+	ObjectId = Schema.ObjectId;
+var User = new Schema({
+	name:String
+});
+var User = mongoose.model('User', User);
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  User.find({}, function(err, docs){
+	 console.log(docs); 
+  });
+});
+router.get('/add', function(req, res) {
+	var user = new User(req.params.name);
+	user.save(function(err){
+		if(!err){
+			console.log('add new user');
+		}else{
+			throw err;
+		}
+	});
+});
+
+module.exports = router;
